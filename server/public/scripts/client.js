d9f2eef1356cdf4f4f09017f400eff0e43379541
Kristen Hare <sailorgoon@gmail.com>
2018-04-22 00:30:53 -0500

    add new object to array, array in calcmod module, app.get and app.post added

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,15 +5,44 @@ $(document).ready(onReady);
 function onReady() {
     console.log('jquery is loaded')
     $('#submitBtn').on('click', clickHandler);
+    getAll();
 }
 
 function clickHandler(){
     console.log('in clickHandler');
-    prependToDom();
+    addNew();
 }
 
-function prependToDom(){
-    $('#outputDiv').prepend("<p>" + $('#number1').val() +
-    $('#operator').val() +
-    $('#number2').val() + " =" + "</p>")
+function getAll(){
+    $.ajax({
+        method: 'GET',
+        url: '/calculations'
+    })
+
+    .then(function(response) {
+        console.log(response);
+        // $('#outputDiv').empty();
+        response.forEach(function(calc){
+            $('#outputDiv').prepend(`<p>${calc.x} ${calc.type} ${calc.y}`);
+        });
+    });
+}
+
+function addNew(){
+const newCalc = {
+    x: $('#number1').val(),
+    y: $('#number2').val(),
+    type: $('#operator').val()
+};
+console.log('new calc object', newCalc);
+
+$.ajax({
+    method: 'POST',
+    url: '/addCalc',
+    data: newCalc
+})
+.then(function(response){
+    console.log(response)
+    getAll();
+});
 }
--- server/server.js
+++ server/server.js
@@ -3,10 +3,19 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+const allCalculations = require('./modules/calcmod')
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/calculations', (req, res) => {
+ res.send(allCalculations); 
+});
 
+app.post('/addCalc', (req, res) => {
+    console.log(req.body);
+    allCalculations.push(req.body);
+        res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
     console.log('listening on 5000');
