bb4739ebd61c73534388233fe368be59c8e9ae62
Kristen Hare <sailorgoon@gmail.com>
2018-04-21 23:09:38 -0500

    prepend input values to dom

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,16 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jquery is loaded')
+    $('#submitBtn').on('click', clickHandler);
+}
+
+function clickHandler(){
+    console.log('in clickHandler');
+    prependToDom();
+}
+
+function prependToDom(){
+    $('#outputDiv').prepend("<p>" + $('#number1').val() +
+    $('#operator').val() +
+    $('#number2').val() + " =" + "</p>")
 }
