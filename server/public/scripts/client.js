96bf5b981477a46f6fbb61201b9e5afe5c83382e
Kristen Hare <sailorgoon@gmail.com>
2018-04-22 20:12:18 -0500

    get rid of commented out non-workingness in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,10 +5,6 @@ $(document).ready(onReady);
 function onReady() {
     console.log('jquery is loaded')
     $(".calcButton").on('click', clickHandler)
-    // $('#addBtn').on('click', addClickHandler);
-    // $('#subtractBtn').on('click', subtractClickHandler);
-    // $('#multiplyBtn').on('click', multiplyClickHandler);
-    // $('#divideBtn').on('click', divideClickHandler);
     getAll();
     $("#clearBtn").on('click', clearAll)
 
@@ -17,34 +13,6 @@ function onReady() {
 function clickHandler() {
     console.log('in clickHandler');
     addToHistory($(this).text());
-    // answerFinder();
-}
-
-
-
-
-//  function answerFinder(){
-//     if($('#operator').val() == "+"){
-//         const answer = Number($('#number1').val()) + Number($('#number2').val());
-//         $('#answerSpan').text( answer );
-//     console.log( answer );
-//     }
-
-//     else if($('#operator').val() == "*"){
-//         const answer = Number($('#number1').val()) * Number($('#number2').val());
-//         $('#answerSpan').text( answer );
-//     }
-
-//     else if($('#operator').val() == "-"){
-//         const answer = Number($('#number1').val()) - Number($('#number2').val());
-//         $('#answerSpan').text( answer );
-//     }
-
-//     else if($('#operator').val() == "/"){
-//         const answer = Number($('#number1').val()) / Number($('#number2').val());
-//         $('#answerSpan').text( answer );
-//     }
-//  }
 
 
 function getAll() {
